06ceb125307c1f9b33da2a1a2058c5ee283062fd
DESKTOP-4KBQ6U4\사용자 <backcommail@gmail.com>
2026-02-10 08:24:35 +0900

    기존파일에 새로운 파일 추가

---
+++ 26-02-02/Source/Project/MyAIController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+#include "MyCharacter.h"
+
+void AMyCharacter* Character = Cast<AMyCharacter>(OtherActor)
+{
+
+}
